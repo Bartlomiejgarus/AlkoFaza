e7afb3d3cb63947b7837b07392da1752be634b78
bartekk.garus@gmail.com <Mikolaj1/>
2023-05-17 22:36:59 +0200

    sample HTML forms

---
+++ public/views/editPost.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/stylePosts.css">
+    <title>posts</title>
+</head> 
+
+<body>
+    <div class="base-container">
+        <nav>
+            <img src="public/img/logo.svg">
+            <ul>
+                <li>
+                    <i class="fas"></i>
+                    <a href="#" class="button">Posts</a>
+                </li>
+                <li>    
+                    <i class="fas"></i>
+                    <a href="#" class="button"> Users</a>
+                </li>
+                <li>
+                    <i class="fas"></i>
+                    <a href="#" class="button-logout">Log out</a>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <section class="editPost">
+                <h1>UPLOAD</h1>
+                <form action="editPost" method="POST" ENCTYPE="multipart/form-data">
+                    <div class="messages">
+                        <?php
+                            if(isset($messages)){
+                                foreach($messages as $message) {
+                                    echo $message;
+                                }
+                            }
+                        ?>
+                    </div>
+                    <img src="public/uploads/long-island-iced-tea.jpg">
+                    <input name="title" type="text" placeholder="title">
+                    <textarea name="ingredients" rows=5 placeholder="ingredients"></textarea>
+                    <textarea name="description" rows=5 placeholder="description"></textarea>
+                    <button type="submit">send</button>
+                </form>
+            </section>
+            <header>
+                <div class="search-bar">
+                    <form>
+                        <input placeholder="search">
+                    </form>
+                </div>
+                <button class="add">
+                    ADD
+                </button>
+            </header>
+        </main>
+    </div>
+</body>
---
+++ public/views/showPost.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/stylePosts.css">
+    <title>posts</title>
+</head> 
+
+<body>
+    <div class="base-container">
+        <nav>
+            <img src="public/img/logo.svg">
+            <ul>
+                <li>
+                    <i class="fas"></i>
+                    <a href="#" class="button">Posts</a>
+                </li>
+                <li>    
+                    <i class="fas"></i>
+                    <a href="#" class="button"> Users</a>
+                </li>
+                <li>
+                    <i class="fas"></i>
+                    <a href="#" class="button-logout">Log out</a>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <section class="showPost">
+                <h1>UPLOAD</h1>
+                <form action="showPost" method="POST" ENCTYPE="multipart/form-data">
+                    <img src="public/uploads/long-island-iced-tea.jpg">
+                    <input name="title" type="text" placeholder="title">
+                    <textarea name="ingredients" rows=5 placeholder="ingredients"></textarea>
+                    <textarea name="description" rows=5 placeholder="description"></textarea>
+                    <button type="submit">send</button>
+                </form>
+            </section>
+            <header>
+                <div class="search-bar">
+                    <form>
+                        <input placeholder="search">
+                    </form>
+                </div>
+                <button class="add">
+                    ADD
+                </button>
+            </header>
+        </main>
+    </div>
+</body>
